f7679a70bb4f7912f1c1b8d3c541a75fb2ca104f
Ben <ncor89b@gmail.com>
2023-12-05 21:45:46 +0100

    build(trending.ts): Create data for displaying in app

---
+++ data/trending.ts
@@ -0,0 +1,72 @@
+export const destinations_data = [
+  {
+    id: 1,
+    src: "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/France/Paris/paris-attractions-xlarge.jpg",
+    title: "Paris",
+    location: "France",
+    description: "Paris is the capital and most populous city of France."
+  },
+  {
+    id: 2,
+    src: "https://media.istockphoto.com/id/537856787/fr/photo/paysage-de-la-toscane-vallonn%C3%A9es.webp?b=1&s=170667a&w=0&k=20&c=G_FcRAXZBGmWrSbNy62Uu09THwZVl8YPCFkU93F-7u4=",
+    capital: "Rome",
+    location: "Italy",
+    description: "Rome is the capital city and a special comune of Italy."
+  },
+  {
+    id: 3,
+    src: "https://media.istockphoto.com/id/1411691139/fr/photo/vue-%C3%A0-travers-une-fen%C3%AAtre-ouverte-avec-des-volets-de-la-plage-de-sable-de-la-c%C3%B4te-rocheuse-et.webp?b=1&s=170667a&w=0&k=20&c=OzdIJEm0AWf8NlavlkXZWIIxMpFSglmab5p6patV4Bg=",
+    capital: "Madrid",
+    location: "Spain",
+    description: "Madrid is the capital and most populous city of Spain."
+  },
+  {
+    id: 4,
+    src: "https://media.istockphoto.com/id/1639008607/fr/photo/horizon-de-la-ville-de-berlin-au-pont-oberbaum-et-%C3%A0-la-rivi%C3%A8re-spree-en-allemagne.webp?b=1&s=170667a&w=0&k=20&c=8l2vi_H1X32lhnVU01ZZTJug_bDi6URDnHHwv6Cp2yk=",
+    capital: "Berlin",
+    location: "Germany",
+    description: "Berlin is the capital and largest city of Germany."
+  },
+  {
+    id: 5,
+    src: "https://media.istockphoto.com/id/1086212768/fr/photo/lac-des-sept-villes-lagoa-das-sete-cidades-%C3%AEle-de-sao-miguel-a%C3%A7ores-portugal.webp?b=1&s=170667a&w=0&k=20&c=OcKDIVl1GPBxYVKSaWrc3GxlJ8DlWuYL999eGgbx9sY=",
+    capital: "Lisbon",
+    location: "Portugal",
+    description: "Lisbon is the capital and the largest city of Portugal."
+  },
+  {
+    id: 6,
+    src: "https://media.istockphoto.com/id/1038943424/fr/photo/vue-panoramique-du-lago-di-braies-dans-les-dolomites.webp?b=1&s=170667a&w=0&k=20&c=zT90l2riH6RPgtMfNK45obgXbqnWxHE31eZ6zYADqiU=",
+    capital: "Vienna",
+    location: "Austria",
+    description: "Vienna is the federal capital and largest city of Austria."
+  },
+  {
+    id: 7,
+    src: "https://media.istockphoto.com/id/886760558/fr/photo/panorama-de-la-baie-du-naufrage-plage-l%C3%AEle-de-zakynthos-gr%C3%A8ce.webp?b=1&s=170667a&w=0&k=20&c=Vcn_cB6-ifBHFkkkReALMLn9ZCoIjtrBd9jx_aZA7Ts=",
+    capital: "Athens",
+    location: "Greece",
+    description: "Athens is the capital and largest city of Greece."
+  },
+  {
+    id: 8,
+    src: "https://media.istockphoto.com/id/885965442/fr/photo/b%C3%A2timents-anciens-traditionnels-et-les-tulipes-%C3%A0-amsterdam-pays-bas.webp?b=1&s=170667a&w=0&k=20&c=wn_aLOLNoPpABnDLcOHsfrXteip562oMbEO-v8t9OXI=",
+    capital: "Amsterdam",
+    location: "Netherlands",
+    description: "Amsterdam is the capital and most populous city of the Netherlands."
+  },
+  {
+    id: 9,
+    src: "https://www.brussels.be/sites/default/files/styles/hero/public/field/image/bruxelles_0.jpg?itok=ZQ1zjJZt",
+    capital: "Brussels",
+    location: "Belgium",
+    description: "Brussels is the capital city of Belgium."
+  },
+  {
+    id: 10,
+    src: "https://www.irishtimes.com/polopoly_fs/1.4620762.1625221664!/image/image.jpg_gen/derivatives/landscape_620/image.jpg",
+    capital: "Dublin",
+    location: "Ireland",
+    description: "Dublin is the capital and largest city of Ireland."
+  }
+];
