fb9f8a20a92ec711cbb13dbc4fa0452bbd786eda
Ben <ncor89b@gmail.com>
2023-12-06 21:19:31 +0100

    build(trending.ts): Update data and values for displaying in app

--- data/trending.ts
+++ data/trending.ts
@@ -9,63 +9,63 @@ export const destinations_data = [
   {
     id: 2,
     src: "https://media.istockphoto.com/id/537856787/fr/photo/paysage-de-la-toscane-vallonn%C3%A9es.webp?b=1&s=170667a&w=0&k=20&c=G_FcRAXZBGmWrSbNy62Uu09THwZVl8YPCFkU93F-7u4=",
-    capital: "Rome",
+    title: "Rome",
     location: "Italy",
     description: "Rome is the capital city and a special comune of Italy."
   },
   {
     id: 3,
     src: "https://media.istockphoto.com/id/1411691139/fr/photo/vue-%C3%A0-travers-une-fen%C3%AAtre-ouverte-avec-des-volets-de-la-plage-de-sable-de-la-c%C3%B4te-rocheuse-et.webp?b=1&s=170667a&w=0&k=20&c=OzdIJEm0AWf8NlavlkXZWIIxMpFSglmab5p6patV4Bg=",
-    capital: "Madrid",
+    title: "Madrid",
     location: "Spain",
     description: "Madrid is the capital and most populous city of Spain."
   },
   {
     id: 4,
-    src: "https://media.istockphoto.com/id/1639008607/fr/photo/horizon-de-la-ville-de-berlin-au-pont-oberbaum-et-%C3%A0-la-rivi%C3%A8re-spree-en-allemagne.webp?b=1&s=170667a&w=0&k=20&c=8l2vi_H1X32lhnVU01ZZTJug_bDi6URDnHHwv6Cp2yk=",
-    capital: "Berlin",
+    src: "https://images.unsplash.com/photo-1533834181702-758cc2177c75?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Berlin",
     location: "Germany",
     description: "Berlin is the capital and largest city of Germany."
   },
   {
     id: 5,
     src: "https://media.istockphoto.com/id/1086212768/fr/photo/lac-des-sept-villes-lagoa-das-sete-cidades-%C3%AEle-de-sao-miguel-a%C3%A7ores-portugal.webp?b=1&s=170667a&w=0&k=20&c=OcKDIVl1GPBxYVKSaWrc3GxlJ8DlWuYL999eGgbx9sY=",
-    capital: "Lisbon",
+    title: "Lisbon",
     location: "Portugal",
     description: "Lisbon is the capital and the largest city of Portugal."
   },
   {
     id: 6,
     src: "https://media.istockphoto.com/id/1038943424/fr/photo/vue-panoramique-du-lago-di-braies-dans-les-dolomites.webp?b=1&s=170667a&w=0&k=20&c=zT90l2riH6RPgtMfNK45obgXbqnWxHE31eZ6zYADqiU=",
-    capital: "Vienna",
+    title: "Vienna",
     location: "Austria",
     description: "Vienna is the federal capital and largest city of Austria."
   },
   {
     id: 7,
     src: "https://media.istockphoto.com/id/886760558/fr/photo/panorama-de-la-baie-du-naufrage-plage-l%C3%AEle-de-zakynthos-gr%C3%A8ce.webp?b=1&s=170667a&w=0&k=20&c=Vcn_cB6-ifBHFkkkReALMLn9ZCoIjtrBd9jx_aZA7Ts=",
-    capital: "Athens",
+    title: "Athens",
     location: "Greece",
     description: "Athens is the capital and largest city of Greece."
   },
   {
     id: 8,
     src: "https://media.istockphoto.com/id/885965442/fr/photo/b%C3%A2timents-anciens-traditionnels-et-les-tulipes-%C3%A0-amsterdam-pays-bas.webp?b=1&s=170667a&w=0&k=20&c=wn_aLOLNoPpABnDLcOHsfrXteip562oMbEO-v8t9OXI=",
-    capital: "Amsterdam",
+    title: "Amsterdam",
     location: "Netherlands",
     description: "Amsterdam is the capital and most populous city of the Netherlands."
   },
   {
     id: 9,
-    src: "https://www.brussels.be/sites/default/files/styles/hero/public/field/image/bruxelles_0.jpg?itok=ZQ1zjJZt",
-    capital: "Brussels",
+    src: "https://media.istockphoto.com/id/1326041533/fr/photo/vue-de-la-pittoresque-ville-de-dinant-belgique.webp?b=1&s=170667a&w=0&k=20&c=_YZuuJFVrEsx7vWBFCaOh9PXBmiWt9TMrpmdneM8JBY=",
+    title: "Brussels",
     location: "Belgium",
     description: "Brussels is the capital city of Belgium."
   },
   {
     id: 10,
-    src: "https://www.irishtimes.com/polopoly_fs/1.4620762.1625221664!/image/image.jpg_gen/derivatives/landscape_620/image.jpg",
-    capital: "Dublin",
+    src: "https://media.istockphoto.com/id/1291582453/fr/photo/falaises-de-moher-en-irlande.webp?b=1&s=170667a&w=0&k=20&c=oChT31FGAoGSdc4jTvlR5O2S1JA-TeCUXkKFhlusltY=",
+    title: "Dublin",
     location: "Ireland",
     description: "Dublin is the capital and largest city of Ireland."
   }
