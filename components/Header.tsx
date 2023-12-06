536258cbe44592151cbf1d447601751a8a426c92
Ben <ncor89b@gmail.com>
2023-12-06 21:14:49 +0100

    build(Header.tsx): Implementing new font family in different parts of component

--- components/Header.tsx
+++ components/Header.tsx
@@ -74,7 +74,7 @@ function Header() {
 
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
-            <Popover.Button className="outline-none border-none flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
+            <Popover.Button className="outline-none border-none flex items-center gap-x-1 text-sm font-['Rubik'] font-normal leading-6 text-white">
               Search Stays
               <ChevronDownIcon 
                 className='h-5 w-5 flex-none text-white' aria-hidden="true"
@@ -135,22 +135,22 @@ function Header() {
             </Transition>
           </Popover>
 
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
+          <a href="#" className="text-sm font-['Rubik'] font-normal leading-6 text-white">
             Flights
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
+          <a href="#" className="text-sm font-['Rubik'] font-normal leading-6 text-white">
             Car Rentals
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
+          <a href="#" className="text-sm font-['Rubik'] font-normal leading-6 text-white">
             Flight & Hotel
           </a>
         </Popover.Group>
 
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="flex text-sm font-semibold leading-6 text-white">
+          <a href="#" className="flex text-sm font-['Rubik'] font-normal leading-6 text-white">
             Log In
             <span aria-hidden="true">
-              <LogIn className='h-6 w-6 ml-2' />
+              <LogIn className='h-5 w-5 ml-2 pt-1' />
             </span>
           </a>
         </div>
