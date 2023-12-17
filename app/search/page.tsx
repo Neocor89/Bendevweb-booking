ec35ecdfa8c9441f92b105dfba1b238a366ffe8c
Ben <ncor89b@gmail.com>
2023-12-17 18:48:13 +0100

    build(SearchPage): Creation of search page and URL decomposition for data retrieval

---
+++ app/search/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation";
+// import fetchResults from "../../lib/fetchresults";
+
+type Props = {
+  searchParams: SearchParams;
+}
+
+type SearchParams = {
+  group_adults: string;
+  url: URL;
+  checkin: string;
+  checkout: string;
+  group_children: string;
+  no_rooms: string;
+}
+
+async function SearchPage({searchParams}: Props) {
+  if (!searchParams.url) return notFound();
+
+  // const results = await fetchResults(searchParams);
+
+  // if (!results) return <div>Not results found</div>;
+
+  return (
+    <div>SearchPage</div>
+  )
+}
+
+export default SearchPage
