3a0dc0a645d06ef165456aeaaf035b31422392a9
Ben <ncor89b@gmail.com>
2024-01-12 23:23:35 +0100

    build(next.config.js): Add new image domains in app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.builder.io", "cf.bstatic.com"],
+    domains: ["cdn.builder.io", "cf.bstatic.com", "media.istockphoto.com"],
   },
 };
 
