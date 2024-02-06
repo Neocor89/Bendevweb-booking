8133fc4655973b6a9de4c855971d707d2fa926e3
Ben <ncor89b@gmail.com>
2024-02-06 21:51:10 +0100

    style(fetchResults.ts): Add code comments

---
+++ lib/fetchresults.ts
@@ -0,0 +1,16 @@
+// import fsPromises from 'fs/promises';
+// import path from 'path';
+
+// export async function fetchResults() {
+  // Obtenez le chemin du fichier JSON
+  // const filePath = path.join(process.cwd(), 'data.json');
+
+  // Lisez le fichier JSON
+  // const jsonData = await fsPromises.readFile(filePath);
+
+  // Analysez les données en tant qu'objet JSON
+  // const objectData = JSON.parse(jsonData);
+
+  // Retournez les données pour les utiliser dans votre application
+  // return objectData;
+// }
