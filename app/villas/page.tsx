4993afecddff21a5b243f1cd6b32eb73663f41dc
Ben <ncor89b@gmail.com>
2024-02-09 21:05:20 +0100

    build(page.tsx): Change structure of villas page

--- app/villas/page.tsx
+++ app/villas/page.tsx
@@ -1,11 +1,12 @@
+import { recommanded_data } from "@/data/recommended";
 import { useRouter } from "next/router";
-import recommanded_data from "@/data/recommended";
+
 
 export default function VillaDetails() {
   const router = useRouter();
   const { id } = router.query; // Récupère l'identifiant de la villa depuis les paramètres d'URL
 
-  // const villa = recommanded_data.find((villa) => villa.id === Number(id));
+  const villas = recommanded_data.find((villa) => villa.id === Number(id));
 
   // Utilisez l'identifiant pour récupérer les détails de la villa depuis votre source de données (par exemple, une API ou une base de données)
   // Affichez les détails de la villa ici
@@ -13,7 +14,7 @@ export default function VillaDetails() {
   return (
     <div>
       {/* <h1>Détails de la villa {id}</h1> */}
-      <p>Titre : {recommanded_data}</p>
+      <p>Titre : {villas?.description}</p>
       {/* Affichez les autres informations de la villa ici */}
     </div>
   );
